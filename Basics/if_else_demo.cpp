608974d20705d573ea5fabc1861d3600f1c55cd3
Nikhil Kumar Jha <128040919+nikhil11690@users.noreply.github.com>
2026-05-23 14:09:42 +0530

    Add if-else demo in if_else_demo.cpp

---
+++ Basics/if_else_demo.cpp
@@ -0,0 +1,16 @@
+// If-Else Demo
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = 1;
+
+    if(++a){
+        cout << b;
+    }
+    else{
+        cout << ++b;
+    }
+}
