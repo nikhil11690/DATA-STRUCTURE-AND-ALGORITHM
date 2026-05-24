07ffc9bc13f04e431ad43df09beb9c62ae4a42d5
Nikhil Kumar Jha <kumarjhanikhil31@gmail.com>
2026-05-25 00:35:43 +0530

    Create mountain_peak.cpp

---
+++ Binary_search/mountain_peak.cpp
@@ -0,0 +1,19 @@
+// peak of a mountain
+#include <bits/stdc++.h> 
+int peakValue(vector<int> &arr){
+
+       int start = 0;
+       int end = arr.size()-1;
+       int mid = start + (end-start)/2;
+       while(start<end){
+           if(arr[mid]<arr[mid+1]){
+               start = mid+1;
+           }
+           else{
+               end = mid;
+           }
+            mid = start + (end-start)/2;
+       }
+       return start;
+
+}
