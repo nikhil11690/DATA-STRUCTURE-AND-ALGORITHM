ab8eebf42b1b9c6e2f073a2d7290b19e8b7e3f3a
Nikhil Kumar Jha <128040919+nikhil11690@users.noreply.github.com>
2026-05-24 12:25:28 +0530

    Create operators.cpp

---
+++ Basics/operators.cpp
@@ -0,0 +1,17 @@
+// Bitwise Operators Demo
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 6;
+    int b = 4;
+
+    cout << "a & b: " << (a & b) << endl;
+    cout << "a | b: " << (a | b) << endl;
+    cout << "a ^ b: " << (a ^ b) << endl;
+    cout << "~a: " << (~a) << endl;
+
+    cout << "a << b: " << (a << b) << endl;
+    cout << "a >> b: " << (a >> b) << endl;
+}
