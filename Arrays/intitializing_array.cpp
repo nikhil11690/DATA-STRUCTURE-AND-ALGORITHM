d1e9d776d2a6b9c09d13cdbc02e5f123713dac17
Nikhil Kumar Jha <128040919+nikhil11690@users.noreply.github.com>
2026-05-24 12:36:39 +0530

    Add array initialization and printing example
    Initialize all elements of an array to 1 and print them.

---
+++ Arrays/intitializing_array.cpp
@@ -0,0 +1,16 @@
+// arrays this code helps of initialize every element with 1 
+#include<iostream>
+using namespace std;
+int main(){
+    int v[15]= {1};
+    for(int i = 0; i<15; i++){
+        v[i] = 1;
+    }
+    for(int i=0;i < 15;i++){
+         cout<<v[i]<<" ";
+
+    }
+
+    return 0;
+
+}
