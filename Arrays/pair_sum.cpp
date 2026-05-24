76420449a55d5c09f168b09ae5f86a6886fc8e79
Nikhil Kumar Jha <128040919+nikhil11690@users.noreply.github.com>
2026-05-24 12:55:39 +0530

    Implement pair sum function to find pairs in array

---
+++ Arrays/pair_sum.cpp
@@ -0,0 +1,26 @@
+//pair sum
+
+#include<iostream>
+using namespace std;
+ void twosum(int arr[],int size,int target){
+    for(int i = 0; i<size;  i++ ){
+        for(int j = i+1; j<size; j++){
+            if(arr[i]+arr[j] == target){
+                cout<<arr[i]<< ","<<arr[j]<<endl;
+            }
+        }
+
+    }
+ }
+
+
+
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    twosum(arr,5,5);
+
+    return 0;
+
+
+}
