1ea408cfd9abb206294e0d99ba95d7cb10c64947
Nikhil Kumar Jha <kumarjhanikhil31@gmail.com>
2026-06-07 13:32:51 +0530

    Add comment for pair difference problem
    Added a comment explaining the purpose of the code.

--- Arrays/pair_with_dffrence_k.cpp
+++ Arrays/pair_with_dffrence_k.cpp
@@ -1,1 +1,4 @@
 
+// You are given with an array of integers and an integer K. You have to find and print the count of all such pairs which have difference K.
+
+// Note: Take absolute difference between the elements of the array.
