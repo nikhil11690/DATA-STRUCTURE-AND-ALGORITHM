54167f3c44140137eb3c7e9230be1a07e4592ac9
Nikhil Kumar Jha <kumarjhanikhil31@gmail.com>
2026-06-08 11:48:07 +0530

    Add pivotArray function to partition array by pivot
    Implement pivotArray function to rearrange elements based on pivot.

---
+++ contest/Partition_Array_According_to_Given_Pivot.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/submissions/detail/2026012326/
+
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> less, equal, greater;
+
+        for (int x : nums) {
+            if (x < pivot) less.push_back(x);
+            else if (x == pivot) equal.push_back(x);
+            else greater.push_back(x);
+        }
+
+        less.insert(less.end(), equal.begin(), equal.end());
+        less.insert(less.end(), greater.begin(), greater.end());
+
+        return less;
+    }
+};
