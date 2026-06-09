e4d6a51c838a60d4375aa586571b4ef82de19f05
Nikhil Kumar Jha <kumarjhanikhil31@gmail.com>
2026-06-09 20:09:17 +0530

    Add function to find duplicate in array using cycle detection
    Implemented Floyd's Tortoise and Hare algorithm to find the duplicate element in an array.

--- contest/Find_Duplicate_in_Array.cpp
+++ contest/Find_Duplicate_in_Array.cpp
@@ -3,3 +3,24 @@
 // Your task is to find the duplicate element. The duplicate element may be repeated more than twice in the error, but there will be exactly one element that is repeated in the array.
 // Note :
 // All the integers in the array appear only once except for precisely one integer which appears two or more times.
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+    int slow = arr[0];
+    int fast = arr[0];
+
+    // Find intersection point
+    do {
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    // Find entrance to the cycle
+    slow = arr[0];
+    while (slow != fast) {
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    return slow;
+}
