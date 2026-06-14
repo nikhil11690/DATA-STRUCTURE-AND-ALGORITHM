47d3e6f08e96433da9cc8598b9eb2785cc9e20ef
Nikhil Kumar Jha <kumarjhanikhil31@gmail.com>
2026-06-14 11:52:43 +0530

    Add total waviness range calculation
    Implement methods to calculate waviness of numbers.

---
+++ contest/total_waiveness_range.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/submissions/detail/2032505165/
+class Solution {
+public:
+    int getWaviness(int num) {
+        string s = to_string(num);
+
+        if (s.size() < 3)
+            return 0;
+
+        int cnt = 0;
+
+        for (int i = 1; i < s.size() - 1; i++) {
+            if ((s[i] > s[i - 1] && s[i] > s[i + 1]) ||
+                (s[i] < s[i - 1] && s[i] < s[i + 1])) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    int totalWaviness(int num1, int num2) {
+        int ans = 0;
+
+        for (int x = num1; x <= num2; x++) {
+            ans += getWaviness(x);
+        }
+
+        return ans;
+    }
+};
