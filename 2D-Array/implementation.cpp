343a00e9cf394e5a9ae9d6acc4e8ed8504403b04
Nikhil <kumarjhanikhil31@gmail.com>
2026-07-20 14:20:40 +0530

    implement 2darray

---
+++ 2D-Array/implementation.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[3][4];
+
+    // taking input
+
+    for(int row = 0 ; row<3 ;row++){
+        for(int col = 0 ; col<4 ; col++){
+            cin>>arr[row][col];
+        }
+    }
+
+    // to print 
+
+    for(int i = 0 ; i<3 ; i++){
+        for(int j = 0 ; j<4; j++){
+            cout<< arr[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
+
+    return 0;
+
+}
