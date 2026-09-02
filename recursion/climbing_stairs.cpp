54bdcb59ce7f83df3c847a268929f877aee6e76d
Nikhil <kumarjhanikhil31@gmail.com>
2026-09-02 20:37:53 +0530

    problem

---
+++ recursion/climbing_stairs.cpp
@@ -0,0 +1,37 @@
+// leetcode -70 
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<0){
+            return 0;
+        }
+        if(n==0){
+            return 1;
+        }
+        int ans = climbStairs(n-1) + climbStairs(n-2);
+        return ans;
+        
+    }
+};
